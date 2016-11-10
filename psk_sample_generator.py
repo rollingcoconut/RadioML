424cdcbb98933d82a35690164d718416d6f5d79c
Fraida Fund <ffund@nyu.edu>
2016-11-09 22:23:55 -0500

    Added GNU Radio scripts to generate PSK signals

---
+++ psk_sample_generator.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Wed Nov  9 21:56:48 2016
+##################################################
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import numpy
+import time
+
+class top_block(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Top Block")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.signal_gain = signal_gain = 0.02
+        self.samp_rate = samp_rate = 32000
+        self.noise_amplitude = noise_amplitude = 0.01
+        self.ndata = 64
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.digital_psk_mod_0 = digital.psk.psk_mod(
+          constellation_points=4,
+          mod_code="gray",
+          differential=True,
+          samples_per_symbol=2,
+          excess_bw=0.35,
+          verbose=False,
+          log=False,
+          )
+        
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
+        self.s2v_signal = blocks.stream_to_vector(gr.sizeof_gr_complex, self.ndata)
+        self.s2v_noise = blocks.stream_to_vector(gr.sizeof_gr_complex, self.ndata)
+        self.s2v_sn = blocks.stream_to_vector(gr.sizeof_gr_complex, self.ndata)
+        self.blocks_probe_signal_and_noise = blocks.probe_signal_vc(self.ndata)
+        self.blocks_probe_noise_only = blocks.probe_signal_vc(self.ndata)
+        self.blocks_probe_signal_only = blocks.probe_signal_vc(self.ndata)       
+        self.blocks_multiply_const_vxx_3 = blocks.multiply_const_vcc((signal_gain, ))
+        self.blocks_add_xx_0 = blocks.add_vcc(1)
+        self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 2, 1000)), True)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, noise_amplitude, 0)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))    
+        self.connect((self.analog_noise_source_x_0, 0), (self.s2v_noise, 0), (self.blocks_probe_noise_only, 0))    
+        self.connect((self.analog_random_source_x_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_add_xx_0, 0), (self.s2v_sn, 0), (self.blocks_probe_signal_and_noise, 0))    
+        self.connect((self.blocks_multiply_const_vxx_3, 0), (self.blocks_add_xx_0, 0))   
+        self.connect((self.blocks_multiply_const_vxx_3, 0),  (self.s2v_signal, 0), (self.blocks_probe_signal_only, 0)) 
+        self.connect((self.blocks_throttle_0, 0), (self.digital_psk_mod_0, 0))    
+        self.connect((self.digital_psk_mod_0, 0), (self.blocks_multiply_const_vxx_3, 0))    
+
+    def get_signal_gain(self):
+        return self.signal_gain
+
+    def set_signal_gain(self, signal_gain):
+        self.signal_gain = signal_gain
+        self.blocks_multiply_const_vxx_3.set_k((self.signal_gain, ))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+
+    def get_noise_amplitude(self):
+        return self.noise_amplitude
+
+    def set_noise_amplitude(self, noise_amplitude):
+        self.noise_amplitude = noise_amplitude
+        self.analog_noise_source_x_0.set_amplitude(self.noise_amplitude)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
+    tb.start()
+    try:
+        while True:
+          sn = (tb.blocks_probe_signal_and_noise.level())   
+          signal = (tb.blocks_probe_signal_only.level())                   
+          noise = (tb.blocks_probe_noise_only.level())                      
+          signal_power = sum(numpy.abs(signal)**2)/len(signal)
+          noise_power = sum(numpy.abs(noise)**2)/len(signal)
+          print(signal_power)
+          print(noise_power)
+          print(10*numpy.log10(signal_power/noise_power)) 
+          time.sleep(5)                      
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()

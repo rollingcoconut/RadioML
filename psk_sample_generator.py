377ac28dfecd99ce40c670147c18ffb4d56adec1
Kimberly <km1851@nyu.edu>
2016-11-17 13:56:18 -0500

    cmd line args can be parsed, and pickling feature introduced;TODO bash script

--- psk_sample_generator.py
+++ psk_sample_generator.py
@@ -15,20 +15,24 @@ from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import numpy
-import time
-
+import time, sys, argparse
+import pickle
+    
 class top_block(gr.top_block):
 
-    def __init__(self):
+    def __init__(self, noise_amp, ndata):
         gr.top_block.__init__(self, "Top Block")
 
         ##################################################
         # Variables
         ##################################################
-        self.signal_gain = signal_gain = 0.02
+        # NOTE on SNR db value: H/N -> 0.01 / 0.01 results in -3db
+        #                       H/N -> 0.01 / 0.02 results in -9db
+        #                       H/N -> 0.01 / 0.005 results in 3db
+        self.signal_gain = signal_gain = 0.01
         self.samp_rate = samp_rate = 32000
-        self.noise_amplitude = noise_amplitude = 0.01
-        self.ndata = 64
+        self.noise_amplitude = noise_amplitude = noise_amp  #noise_amplitude = noise_amp 
+        self.ndata =  ndata 
 
         ##################################################
         # Blocks
@@ -90,20 +94,62 @@ class top_block(gr.top_block):
 
 
 def main(top_block_cls=top_block, options=None):
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("noise_amp", help="amplitude of noise, ex. 0.01. Signal gain is 0.01, and sample rate is 3200", type=float)
+    parser.add_argument("ndata", help="number of consecutive samples collected at a time, ex. 2048", type=int)
+    parser.add_argument("ntrials", help="number of trials, ex. 2", type=int)
+    args = parser.parse_args()
 
-    tb = top_block_cls()
+    tb = top_block_cls(args.noise_amp, args.ndata)
     tb.start()
+
     try:
-        while True:
+        ntrials=args.ntrials
+        
+        noise_matrix = numpy.ndarray(shape=(ntrials, tb.ndata), dtype=complex)
+        signal_matrix = numpy.ndarray(shape=(ntrials, tb.ndata), dtype=complex)
+        signal_and_noise_matrix = numpy.ndarray(shape=(ntrials, tb.ndata), dtype=complex)
+        
+        signal_power = 0
+        threshold = 1e-16
+        while signal_power < threshold:
+            # Read in samples to discard, because they'll be all zero the first time
+            sn = (tb.blocks_probe_signal_and_noise.level())
+            noise = (tb.blocks_probe_noise_only.level())
+            signal = (tb.blocks_probe_signal_only.level())      
+            signal_power = sum(numpy.abs(signal)**2)/len(signal)
+
+        for i in range(ntrials):
           sn = (tb.blocks_probe_signal_and_noise.level())   
-          signal = (tb.blocks_probe_signal_only.level())                   
           noise = (tb.blocks_probe_noise_only.level())                      
+          signal = (tb.blocks_probe_signal_only.level())  
+
+          noise_matrix[i] = noise
+          signal_matrix[i] = signal
+          signal_and_noise_matrix[i] = signal_matrix[i] + noise_matrix[i]
+
+            
           signal_power = sum(numpy.abs(signal)**2)/len(signal)
           noise_power = sum(numpy.abs(noise)**2)/len(signal)
-          print(signal_power)
-          print(noise_power)
-          print(10*numpy.log10(signal_power/noise_power)) 
-          time.sleep(5)                      
+          #print(signal_power)
+          #print(noise_power)
+          #print(10*numpy.log10(signal_power/noise_power))
+
+          expected_SNR = numpy.log2(tb.signal_gain/tb.noise_amplitude)*6 -3 
+          time.sleep(max(tb.ndata/tb.samp_rate, .1))                      
+
+        #print(signal_matrix)
+        #print(noise_matrix)
+
+       # pickle the matrixes
+        P = [ noise_matrix, signal_matrix, signal_and_noise_matrix]
+        str_SNR=  str(abs(int(expected_SNR)))
+        if expected_SNR < 0: 
+            str_SNR= "neg" + str_SNR
+        filename = str_SNR + "db_" + str(ntrials) + "trials"
+        pickle.dump( P, open(filename+ ".p" , "wb"))
+          
     except EOFError:
         pass
     tb.stop()

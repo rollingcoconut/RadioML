81f415c0c93395061dbe90f244f8699de8a35821
Kimberly <km1851@nyu.edu>
2016-11-18 14:58:47 -0500

    finishing parts 1 2

--- create_pickled.sh
+++ create_pickled.sh
@@ -17,6 +17,9 @@ NOISE_AMP=(.01 .0075 .005 .0025 )
 SNR=(-3 0 3 10)
 ndata=(64 128 256 512 1024 2048 4096)
 
+
+# Make modification so if file already exists don't print it again
+
 for noise_amp in ${NOISE_AMP[*]} ; do
 	for sample_quant in ${ndata[*]} ; do
 		python  psk_sample_generator.py $noise_amp $ntrials $sample_quant
